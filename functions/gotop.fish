accf0523fb98a0aa9301176f7bf92420da3827b2
pradyumna <pady92@gmail.com>
2020-05-12 17:50:53 +0530

    updated capitalisation

--- functions/gotop.fish
+++ functions/gotop.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gotop --wraps='cd ~/repo/Projects' --wraps='cd ~/repos/Projects' --description 'alias gotop=cd ~/repos/Projects'
-  cd ~/repos/Projects $argv;
+function gotop --wraps='cd ~/repo/Projects' --wraps='cd ~/repos/Projects' --wraps='cd ~/repos/projects' --description 'alias gotop=cd ~/repos/projects'
+  cd ~/repos/projects $argv;
 end
