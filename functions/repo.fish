b63c94740a803471867f0be7e231f3ec1fd499e5
Pradyumna <pady92@gmail.com>
2020-07-18 16:15:31 +0530

    modified repo and gotop shortcut to accept arguments, else go to respective base folder

--- functions/repo.fish
+++ functions/repo.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
 function repo --wraps='cd ~/repos' --description 'alias repo=cd ~/repos'
-  cd ~/repos/$argv;
+    if count $argv > /dev/null
+        cd ~/repos/$argv;
+    else
+        cd ~/repos
+    end 
 end
