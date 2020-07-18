a69b3d2ce94340228f47260c87d8a014c0121430
Pradyumna <pady92@gmail.com>
2020-07-18 16:09:32 +0530

    fish new alias and buku updates

--- functions/bukuserve.fish
+++ functions/bukuserve.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function bukuserve --wraps='~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 127.0.0.1 --port 21000' --description 'alias bukuserve ~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 127.0.0.1 --port 21000'
-  ~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 127.0.0.1 --port 21000 $argv;
+function bukuserve --wraps='~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 0.0.0.0 --port 5001' --description 'alias bukuserve ~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 0.0.0.0 --port 5001'
+  ~/repos/buku/env/bin/python ~/repos/buku/env/bin/bukuserver run --host 0.0.0.0 --port 5001 $argv;
 end
