3826fe37da940e30c40ede3d2ee2a5b74fc053f9
Pradyumna <pady92@gmail.com>
2021-02-11 18:31:30 +0530

    updated goton notes path to /home/pi/notes

--- functions/goton.fish
+++ functions/goton.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function goton --wraps='cd ~/repo/notes' --wraps='cd ~/repos/notes' --description 'alias goton=cd ~/repos/notes'
-  cd ~/repos/notes $argv;
+function goton --description 'alias goton cd ~/notes'
+	cd ~/notes $argv;
 end
