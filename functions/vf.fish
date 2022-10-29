b5de6f2596aa37902cc62b7b75cbb791a8e06b3e
Pradyumna <pady92@gmail.com>
2022-10-29 17:42:51 +0530

    modified vf alias to cancel on escape from fzf operation

--- functions/vf.fish
+++ functions/vf.fish
@@ -1,4 +1,11 @@
 # Defined in - @ line 1
 function vf --wraps='v (fzf)' --description 'alias vf v (fzf)'
-  v (fzf) $argv;
+  set __filepath (fzf)
+  if test -n "$__filepath" 
+    echo $__filepath
+    v $__filepath
+  else
+    echo 'Cancelled by user!'
+  end
+  
 end
