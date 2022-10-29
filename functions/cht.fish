c972c97f5ed5b11ed29088b7eea981526b66af68
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-10-29 11:10:46 +0530

    added aliases: gitstatus - cheat.sh(on bat display) - mkrepo

---
+++ functions/cht.fish
@@ -0,0 +1,3 @@
+function cht --wraps='cht.sh  | bat' --description 'alias cht cht.sh  | bat'
+  cht.sh  | bat $argv; 
+end
---
+++ functions/mkrepo.fish
@@ -0,0 +1,5 @@
+function mkrepo 
+  # echo $argv[1]
+  mkdir ~/repos/$argv[1];
+  cd ~/repos/$argv[1]; 
+end
