574b9830a67b274ef3657c304337f5a764f35b30
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-10-31 11:28:29 +0530

    added syntax highlighting to jp

---
+++ functions/rainbowstream.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function rainbowstream --description 'alias rainbowstream ./softenvpy/bin/rainbowstream'
+	~/softenvpy/bin/rainbowstream  $argv;
+end
---
+++ functions/tuir.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function tuir --description 'alias tuir ./softenvpy/bin/tuir'
+	~/softenvpy/bin/tuir  $argv;
+end
