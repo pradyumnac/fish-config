b5da053a204777b6ced729564521ca0efbcd3507
Pradyumna <pady92@gmail.com>
2022-11-02 13:05:39 +0530

    modified bat alias to first look for bat command, if not available then use batcat

--- functions/bat.fish
+++ functions/bat.fish
@@ -1,3 +1,7 @@
-function bat --wraps=batcat --description 'alias bat batcat'
-  batcat $argv; 
+function bat 
+  if command -sq bat; 
+     eval (which bat) $argv;
+  else
+    batcat $argv; 
+  end
 end
