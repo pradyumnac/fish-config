dd9ccd68137246295c57a82e38a4ba98c34a0245
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-11-30 16:23:31 +0530

    mpv/ newsboat/ shellscripts/

--- functions/vf.fish
+++ functions/vf.fish
@@ -1,11 +1,3 @@
-# Defined in - @ line 1
-function vf --wraps='v (fzf)' --description 'alias vf v (fzf)'
-  set __filepath (fzf)
-  if test -n "$__filepath" 
-    echo $__filepath
-    v $__filepath
-  else
-    echo 'Cancelled by user!'
-  end
-  
+function vf --wraps='v (fzf)' --wraps='v -p (fzf)' --description 'alias vf v -p (fzf)'
+  v -p (fzf) $argv; 
 end
