34e8deff4916de1056b5b4c0c8d1a4f6bc22fc2a
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-11-30 17:16:06 +0530

    vf fixed and tested

--- functions/vf.fish
+++ functions/vf.fish
@@ -1,9 +1,8 @@
 function vf 
   set __filepaths (fzf)
-  if test -n "$__filepath" 
-  then 
-    echo $__filepath
-    v -p $__filepath
+  if test -n "$__filepaths" 
+    echo $__filepaths
+    v -p $__filepaths
   else
     echo 'Cancelled by user!'
   end
