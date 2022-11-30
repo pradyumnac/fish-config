b137f7a5a09ba8fb761f9ee07f34af24e90dbfe9
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-11-30 16:32:24 +0530

    vf now supports mullti tab open

--- functions/vf.fish
+++ functions/vf.fish
@@ -1,3 +1,11 @@
-function vf --wraps='v (fzf)' --wraps='v -p (fzf)' --description 'alias vf v -p (fzf)'
-  v -p (fzf) $argv; 
+# Defined in - @ line 1
+function vf --wraps='v (fzf)' --description 'alias vf v (fzf)'
+  set __filepaths (fzf)
+  if -z "$__filepath" 
+    echo $__filepath
+    v -p $__filepath
+  else
+    echo 'Cancelled by user!'
+  end
+  
 end
