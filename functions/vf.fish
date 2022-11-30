0495bef305afd62b0c0b01feae5c00602d4d2c7e
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-11-30 17:11:06 +0530

    fixed fsh vf if syntax

--- functions/vf.fish
+++ functions/vf.fish
@@ -1,11 +1,11 @@
-# Defined in - @ line 1
-function vf --wraps='v (fzf)' --description 'alias vf v (fzf)'
+function vf 
   set __filepaths (fzf)
-  if -z "$__filepath" 
+  if test -n "$__filepath" 
+  then 
     echo $__filepath
     v -p $__filepath
   else
     echo 'Cancelled by user!'
   end
-  
+
 end
