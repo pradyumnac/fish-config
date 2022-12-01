c595893ef8467f6a40fc1063cc3158f04d854d4c
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-01 20:15:33 +0530

    cleaned up fish wraps in aliases in pusha pulla

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,3 +1,3 @@
-function pulla --wraps='set current_dir (pwd);cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone;cd ' --description 'alias pulla set current_dir (pwd);cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone;cd '
+function pulla 
   set current_dir (pwd);cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone;cd $current_dir; 
 end
