a209ae1e5e1ae8fcff9c6f523f2bd51df0f5d3b8
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-01 21:24:43 +0530

    "aliases" function puts the alias name to type out args now

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -1,3 +1,3 @@
-function aliases --wraps='ls ~/repos/fish/functions|gum filter' --wraps=eval\ \(ls\ \~/repos/fish/functions\|gum\ filter\|sed\ \'s/\\.fish/\ /g\'\) --description alias\ aliases\ eval\ \(ls\ \~/repos/fish/functions\|gum\ filter\|sed\ \'s/\\.fish/\ /g\'\)
-  eval (ls ~/repos/fish/functions|gum filter|sed 's/\.fish/ /g') $argv; 
+function aliases
+  commandline -r -- (ls ~/repos/fish/functions|gum filter|sed 's/\.fish/ /g') 
 end
