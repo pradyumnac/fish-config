4d85168d360c4e97b4ac7c5231a2c630c41fbcbe
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-01 23:02:47 +0530

    added config.fish from master

--- config.fish
+++ config.fish
@@ -3,25 +3,31 @@ set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 set -x LANG en_UK.UTF-8
 
-
-
 set -gx fish_greeting ''
-set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
 
 # PATH Variables
+test -e ~/.local/bin; and set -gx PATH $PATH ~/.local/bin
+
 test -e ~/.go; and set -gx GOPATH ~/.go
 test -e ~/go; and set -gx GOPATH ~/go
-
 test -e ~/.go/bin; and set -gx PATH $PATH $GOPATH/bin
 test -e ~/go/bin; and set -gx PATH $PATH $GOPATH/bin
-test -e ~/.local/bin; and set -gx PATH $PATH ~/.local/bin
-test -e /opt/wine-staging/bin; and set -gx PATH $PATH /opt/wine-staging/bin
+test -e /usr/local/go/bin; and set -gx PATH $PATH /usr/local/go/bin
 
-# WSl workaround to cd to home path
-# cd 
+test -e ~/.cargo/bin; and set -gx PATH $PATH ~/.cargo/bin
+
+# test -e /opt/wine-staging/bin; and set -gx PATH $PATH /opt/wine-staging/bin
 
 # FZF setup
 # fzf_configure_bindings --git_status=\cg --history=\ch --variables= --processes=
+set -gx FZF_DEFAULT_OPTS '-m --height 50% --border'
+
+if type rg &> /dev/null
+  # set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
+  set -gx FZF_DEFAULT_COMMAND 'rg --files'
+end
 
 # Starship
 starship init fish | source
+
+source ~/repos/env/.gumrc
