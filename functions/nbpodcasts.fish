e20bd9802656afe9e8bc63b7b1c0048609d2be2d
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-02 18:50:10 +0530

    removed nb podcasts in favour of nbp

--- functions/nbpodcasts.fish
+++
@@ -1,3 +0,0 @@
-function nbpodcasts --wraps='newsboat -u ~/repos/newsboat/urlcategory/podcasts' --description 'alias nbpodcasts newsboat -u ~/repos/newsboat/urlcategory/podcasts'
-  newsboat -u ~/repos/newsboat/urlcategory/podcasts $argv; 
-end
