ef1b45fa72f06f44c389dd228acd93ae7e47a593
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-02 18:52:59 +0530

    removed nbyt in favour of nby

--- functions/nbyt.fish
+++
@@ -1,3 +0,0 @@
-function nbyt --wraps='newsboat -u ~/repos/newsboat/urlcategory/youtube' --description 'alias nbyt newsboat -u ~/repos/newsboat/urlcategory/youtube'
-  newsboat -u ~/repos/newsboat/urlcategory/youtube $argv; 
-end
