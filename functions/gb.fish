96c4460a0683059e3aa1ea88a66af69df529c8a6
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-05 17:26:36 +0530

    function: gb for gitbrowse

--- functions/gb.fish
+++ functions/gb.fish
@@ -1,9 +1,28 @@
 function gb --description 'browse git temporarily'
   # git browse
-  mkdir -p ~/repos/.tmp
-  gh repo clone $argv ~/repos/.tmp;
-  repo .tmp
-  v
-  rm ~/repos/.tmp -fR
-  prevd
+  # -k : create a repo and keep
+  # -f : invoke fzf -> nvim
+  
+  set -f options f/fzf k/keep 
+  argparse -n gb $options -- $argv 
+
+  if set -q _flag_keep
+    set -f __foldername (echo $argv|sed -e 's,\(.*\)/,,')
+   else
+    set -f __foldername .tmp   
+  end
+
+  mkdir -p ~/repos/$__foldername
+  cd ~/repos/$__foldername
+
+  echo "creating dir $__foldername and running clone"
+  gh repo clone $argv . 
+  if [ "$status" != "0" ]; echo "clone failed" && return; end
+
+  if set -q _flag_fzf; vf; else; v; end
+
+  prevd 1
+
+  if not set -q _flag_keep; rm -fR ~/repos/$__foldername; end
+
 end
--- functions/gbf.fish
+++
@@ -1,9 +0,0 @@
-function gbf
-  # git browse and open in vim with fzf
-  mkdir -p ~/repos/.tmp
-  gh repo clone $argv ~/repos/.tmp/browse;
-  repo ~/repos/.tmp/browse
-  vf
-  rm ~/repos/.tmp/browse -fR
-  cdh 2
-end
