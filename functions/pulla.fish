05f1f331494320ae0c7d75866bba5a98cdca0d31
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-06 13:07:31 +0530

    added nvim to fish

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,3 +1,5 @@
 function pulla 
-  set current_dir (pwd);cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone;cd $current_dir; 
+  set current_dir (pwd);
+  cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone nvim nvim-config;
+  cd $current_dir; 
 end
