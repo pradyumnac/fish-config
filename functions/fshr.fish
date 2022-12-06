31b7c1d5c5eb5e825aa880dba5faa2e0d7f04d9c
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-06 12:57:31 +0530

    added omf reload to fshr and gb now working with delete confirmation

--- functions/fshr.fish
+++ functions/fshr.fish
@@ -1,3 +1,4 @@
 function fshr --wraps='source ~/repos/fish/config.fish' --description 'alias fshr source ~/repos/fish/config.fish'
   source ~/repos/fish/config.fish $argv; 
+  omf reload
 end
--- functions/gb.fish
+++ functions/gb.fish
@@ -1,28 +1,39 @@
-function gb --description 'browse git temporarily'
+function gb --description 'Browse git'
   # git browse
   # -k : create a repo and keep
   # -f : invoke fzf -> nvim
   
-  set -f options f/fzf k/keep 
+  set -l options f/fzf k/keep 
+  set -l foldername .tmp
+  set -l curdir (pwd)
   argparse -n gb $options -- $argv 
 
   if set -q _flag_keep
-    set -f __foldername (echo $argv|sed -e 's,\(.*\)/,,')
-   else
-    set -f __foldername .tmp   
+    set foldername (echo $argv|sed -e 's,\(.*\)/,,')
   end
 
-  mkdir -p ~/repos/$__foldername
-  cd ~/repos/$__foldername
+  mkdir -p ~/repos/$foldername
+  cd ~/repos/$foldername
+  pwd
 
-  echo "creating dir $__foldername and running clone"
+  echo "creating dir $foldername and running clone"
   gh repo clone $argv . 
-  if [ "$status" != "0" ]; echo "clone failed" && return; end
-
+  if [ "$status" != "0" ]
+    echo "ERROR: Clone failed.. But repo $foldername created."
+    read -l -P "Delete(y/N)?" confirm_yn
+    set confirm_yn (string lower $confirm_yn)
+    if begin [ "$confirm_yn" = "y" ]; or [ "$confirm_yn" = "yes" ]; end
+      repo
+      rm -fR $foldername
+    end
+    cd $curdir
+    return
+  end
+  
   if set -q _flag_fzf; vf; else; v; end
 
-  prevd 1
+  cd $curdir
 
-  if not set -q _flag_keep; rm -fR ~/repos/$__foldername; end
+  if not set -q _flag_keep; rm -fR ~/repos/$foldername; end
 
 end
