4d13076fde84564607871a53d97947ab2bd28d8f
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-06 17:27:58 +0530

    fish alias abbr

--- functions/rpi1.fish
+++ functions/rpi1.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 1
 function rpi1 --wraps='ssh pi@192.168.0.183' --description 'alias rpi1 ssh pi@192.168.0.183'
-  ssh pi@192.168.0.183 $argv;
+  ssh pi@192.168.0.174 $argv;
 end
