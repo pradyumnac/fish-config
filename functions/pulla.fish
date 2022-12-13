462424adf9300cb6b00b69fecd26c4a80290e9c1
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-13 14:57:20 +0530

    removed nvim config from pull repos

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,5 +1,5 @@
-function pulla 
+function pulla
   set current_dir (pwd);
-  cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone nvim nvim-config;
+  cd ~/repos;./pullrepos -c env shellscripts fish newsboat exports mpv tmux rclone nvim; 
   cd $current_dir; 
 end
