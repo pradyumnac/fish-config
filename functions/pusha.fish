2caebbbc2bd080f13994ae16ee6ae59bad39225f
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-14 00:13:09 +0530

    removed shellscripts repo

--- functions/pusha.fish
+++ functions/pusha.fish
@@ -1,3 +1,3 @@
 function pusha
-  set current_dir (pwd);cd ~/repos;./pushrepos -c env shellscripts fish newsboat exports mpv tmux rclone rofi;cd $current_dir; 
+  set current_dir (pwd);cd ~/repos;./pushrepos -c env fish newsboat exports mpv tmux rclone rofi;cd $current_dir; 
 end
