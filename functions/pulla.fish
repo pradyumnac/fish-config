f373cdee0c2a0fd8c23226b644c6c968cdf8068b
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-21 19:20:00 +0530

    added nvim-alt to pulla

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,5 +1,5 @@
 function pulla
   set current_dir (pwd);
-  cd ~/repos;./pullrepos -c env fish newsboat exports mpv tmux rclone nvim rofi; 
+  cd ~/repos;./pullrepos -c env fish newsboat exports mpv tmux rclone nvim nvim-alt rofi; 
   cd $current_dir; 
 end
