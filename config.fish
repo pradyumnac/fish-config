8db811a0418ffb7d951ec199be459a739a9b3df8
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-23 12:02:08 +0530

    prettified config.fish

--- config.fish
+++ config.fish
@@ -2,21 +2,17 @@ set -Ux PAGER less
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 set -x LANG en_IN.UTF-8
-
 set -gx fish_greeting ''
 
 # PATH Variables
 test -e ~/.local/bin; and set -gx PATH $PATH ~/.local/bin
-
 test -e ~/.go; and set -gx GOPATH ~/.go
 test -e ~/go; and set -gx GOPATH ~/go
 test -e ~/.go/bin; and set -gx PATH $PATH $GOPATH/bin
 test -e ~/go/bin; and set -gx PATH $PATH $GOPATH/bin
 test -e /usr/local/go/bin; and set -gx PATH $PATH /usr/local/go/bin
 test -e /snap/bin; and set -gx PATH $PATH /snap/bin
-
 test -e ~/.cargo/bin; and set -gx PATH $PATH ~/.cargo/bin
-
 # test -e /opt/wine-staging/bin; and set -gx PATH $PATH /opt/wine-staging/bin
 
 # FZF setup
@@ -30,5 +26,4 @@ end
 
 # Starship
 starship init fish | source
-
 source ~/repos/env/.gumrc
