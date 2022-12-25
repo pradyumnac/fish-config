8426a4c9d3c584357ebeee634d7ff9967a1d34f4
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-25 11:15:21 +0530

    added func vfl

--- functions/vfl.fish
+++ functions/vfl.fish
@@ -2,7 +2,7 @@ function vfl
   set __filepaths (fzf)
   if test -n "$__filepaths" 
     echo $__filepaths
-    vl -p $__filepaths
+    vla -p $__filepaths
   else
     echo 'Cancelled by user!'
   end
