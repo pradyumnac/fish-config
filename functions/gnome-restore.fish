98737089a15e28f34795990c335179f033770cc3
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-27 21:53:44 +0530

    added aurestore center window under dconf

--- functions/gnome-restore.fish
+++ functions/gnome-restore.fish
@@ -2,5 +2,6 @@ function gnome-restore
   tilix-restore
   gnometerm-restore
   gnomehotkeys-restore
+  dconf load /org/gnome/mutter/ < ~/repos/env/config/gnome-center-window.dconf
   # dconf reset -f /org/gnome/terminal/;dconf load / <  ~/repos/env/config/gnome.ini 
 end
