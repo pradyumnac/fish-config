e3475612fb62b0dc3e9e91b2d826d5f57c948a7b
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-30 00:47:42 +0530

    added nvim-test to pulla

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,5 +1,6 @@
+# Defined in /tmp/fish.vAKDPs/pulla.fish @ line 2
 function pulla
   set current_dir (pwd);
-  cd ~/repos;./pullrepos -c env fish newsboat exports mpv tmux rclone nvim nvim-alt rofi; 
-  cd $current_dir; 
+  cd ~/repos;./pullrepos -c env fish newsboat exports mpv tmux rclone nvim nvim-alt nvim-test rofi ; 
+  cd $current_dir;
 end
