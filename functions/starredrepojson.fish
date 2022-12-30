d606c2a8382b896ec22b50633fa0f8bb194bc2d0
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2022-12-30 15:35:18 +0530

    removed starredrepojson func in fav of ptoject ghstars

--- functions/starredrepojson.fish
+++
@@ -1,3 +0,0 @@
-function starredrepojson --wraps='gh api  -H "Accept: application/vnd.github+json" /user/starred' --description 'alias starredrepojson gh api  -H "Accept: application/vnd.github+json" /user/starred'
-  gh api  -H "Accept: application/vnd.github+json" /user/starred $argv; 
-end
