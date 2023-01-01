c4122b40c0fe226a3f937e3701242cdc26c4ef9e
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-01-01 12:36:19 +0530

    moved order of pulla repos

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.vAKDPs/pulla.fish @ line 2
+# Defined in /tmp/fish.sAXAav/pulla.fish @ line 2
 function pulla
   set current_dir (pwd);
-  cd ~/repos;./pullrepos -c env fish newsboat exports mpv tmux rclone nvim nvim-alt nvim-test rofi ; 
+  cd ~/repos;./pullrepos -c env fish nvim nvim-alt nvim-test newsboat exports mpv tmux rclone rofi ; 
   cd $current_dir;
 end
