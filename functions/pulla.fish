81d2357b38075bf008ed08e90e28bf3a08c49c06
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-01-06 22:22:44 +0530

    modified pulla for push repo local bin instal

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,6 +1,6 @@
 # Defined in /tmp/fish.sAXAav/pulla.fish @ line 2
 function pulla
   set current_dir (pwd);
-  cd ~/repos;./pullrepos -c env fish nvim nvim-alt nvim-test newsboat exports mpv tmux rclone rofi ; 
+  cd ~/repos;pullrepos -c env fish nvim nvim-alt nvim-test newsboat exports mpv tmux rclone rofi ; 
   cd $current_dir;
 end
