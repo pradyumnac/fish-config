9aca777070e7eedf66147462fe8770b3da781795
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-01-14 20:33:11 +0530

    added few repos to pulla

--- functions/pulla.fish
+++ functions/pulla.fish
@@ -1,6 +1,5 @@
-# Defined in /tmp/fish.sAXAav/pulla.fish @ line 2
 function pulla
   set current_dir (pwd);
-  cd ~/repos;pullrepos -c env fish nvim nvim-alt nvim-test newsboat exports mpv tmux rclone rofi ; 
+  cd ~/repos;pullrepos -c env fish nvim nvim-alt nvim-test notes gorss newsboat exports mpv tmux rclone rofi; 
   cd $current_dir;
 end
