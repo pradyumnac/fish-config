d03f1718fbbc201e2fbc16e2c0cd7f2f63639946
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-01-21 17:52:52 +0530

    removed v in fav of bash alias

--- functions/v.fish
+++
@@ -1,9 +0,0 @@
-# Defined in - @ line 1
-function v --wraps=nvim --description 'alias v nvim'
-  if [ -f "/home/doe/appimage/nvim.appimage" ]
-     /home/doe/appimage/nvim.appimage $argv;
-  else
-     nvim  $argv;           
-  end
-  
-end
